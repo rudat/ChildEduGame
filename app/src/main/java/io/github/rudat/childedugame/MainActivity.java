0227e456bbf076bf16dec844d5300c8d7d74409e
Meliphant <op.personal.analytics@gmail.com>
2018-01-21 23:10:04 +0300

    1

--- app/src/main/java/io/github/rudat/childedugame/MainActivity.java
+++ app/src/main/java/io/github/rudat/childedugame/MainActivity.java
@@ -10,6 +10,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
-    //test test
+    //test test khbxkbscblskcbdnb
 }
